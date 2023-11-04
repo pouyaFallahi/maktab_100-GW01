2b28bbd1e706599523e28b8db97dea05d4d85993
Artin <aaturbo1669@gmail.com>
2023-11-04 13:41:56 +0330

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,43 @@
+import unittest
+import sqlite3
+
+# Replace 'your_database.db' with your actual database file
+DATABASE_FILE = 'your_database.db'
+
+
+class TestDatabase(unittest.TestCase):
+
+    def setUp(self):
+        # Set up a database connection and cursor before each test
+        self.conn = sqlite3.connect(DATABASE_FILE)
+        self.cursor = self.conn.cursor()
+
+    def tearDown(self):
+        # Clean up resources after each test
+        self.conn.close()
+
+    def test_table_exists(self):
+        # Test if a specific table exists in the database
+        table_name = 'your_table_name'
+        query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';"
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        self.assertIsNotNone(result)
+
+    def test_data_insertion(self):
+        # Test data insertion into the database
+        data = ("John", "Doe", 30)
+        query = "INSERT INTO your_table_name (first_name, last_name, age) VALUES (?, ?, ?);"
+        self.cursor.execute(query, data)
+        self.conn.commit()
+
+        # Check if the data was inserted correctly
+        query = "SELECT first_name, last_name, age FROM your_table_name WHERE first_name='John';"
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        self.assertIsNotNone(result)
+        self.assertEqual(result, data)
+
+
+if __name__ == '__main__':
+    unittest.main()
