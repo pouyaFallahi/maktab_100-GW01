6c659ef2c0cdd56100b6bf55cd89860f3bb093bf
fa-mahabadi <144953741+fa-mahabadi@users.noreply.github.com>
2023-11-04 20:25:43 +0330

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -10,7 +10,7 @@ class DatabaseTest(unittest.TestCase):
         self.obj.cursor = MagicMock()
 
 
-    def test_create(self):
+    def test_create_table(self):
         values = {
             'admin_name': 'VARCHAR(255) NOT NULL',
             'username': 'VARCHAR(255)',
@@ -22,20 +22,20 @@ class DatabaseTest(unittest.TestCase):
         self.obj.cursor.execute.assert_called_once_with(expected_query)
         self.obj.connection.commit.assert_called_once()
 
-    def test_insert(self):
+    def test_insert_table(self):
         values = {"admin_name": "admin_1", "username": "user", "password": "123456"}
         self.obj.insert("pateint", **values)
         expected_query = "INSERT INTO pateint (admin_name, username, password) VALUES ('admin_1', 'user', '123456');"
         self.obj.cursor.execute.assert_called_once_with(expected_query)
         self.obj.connection.commit.assert_called_once()
 
-    def test_update_with_condition(self):
+    def test_update_table(self):
         self.obj.update("pateint", "username ='user'", admin_name="admin_1", username="usersss", password="123456")
         expected_sql = ("UPDATE pateint SET admin_name = 'admin_1', username = 'usersss', password = '123456' WHERE username ='user';")
         self.obj.execute.assert_called_once_with(expected_sql)
 
 
-    def test_delete_with_condition(self):
+    def test_delete_table(self):
         self.obj.delete("pateint", "username ='usersss'")
         expected_sql = "DELETE FROM pateint WHERE username ='usersss';"
         self.obj.execute.assert_called_once_with(expected_sql)
