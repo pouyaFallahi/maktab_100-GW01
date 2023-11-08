ca4d8e0ecbfba8c0fa9ffb1f54319e048d0b5391
Pouya <66160508+pouyaFallahi@users.noreply.github.com>
2023-11-08 21:57:14 +0330

    update loging_file.py

--- loging.py
+++ loging.py
@@ -5,10 +5,10 @@ class Loging_Admin:
         try:
             self.connection = psycopg2.connect(
                 host="localhost",
-                port="YOUR_PORT",
-                database="NAME_DATABASE",
-                user="POSTGERSQL_USERNAME",
-                password="YOUR_PASSWORD",
+                port="5432",
+                database="hospital",
+                user="pouya",
+                password="1234",
             )
             self.cursor = self.connection.cursor()
 
@@ -51,3 +51,81 @@ class Loging_Admin:
             return "Login unsuccessful. Please check your username and password."
 
 
+class Loging_Patient:
+    def __init__(self):
+        try:
+            self.connection = psycopg2.connect(
+                host="localhost",
+                port="5432",
+                database="hospital",
+                user="pouya",
+                password="1234",
+            )
+            self.cursor = self.connection.cursor()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def loging(self, patient_name, patient_id):
+        self.cursor.execute("SELECT  patient_id FROM patient WHERE patient_name=%s", (patient_name))
+        row = self.cursor.fetchone()
+
+        if row[0] == patient_id and row[1] == patient_name:
+            self.connection.close()
+            return "Login successful!"
+        else:
+            self.connection.close()
+            return "Login unsuccessful. Please check your username and password."
+
+    def register(self, patient_id, patient_name, age):
+        try:
+            self.cursor.execute("INSERT INTO patient (patient_id,patient_name, patient_age) VALUES (%s, %s, %s)",
+                                (patient_id, patient_name, age))
+            self.connection.commit()
+            self.connection.close()
+            return "Registration successful!"
+        except psycopg2.IntegrityError:
+            # نویسنده پویا فلاحی کم حافظه
+            # معمولاً در مواردی ایجاد می‌شوند که تلاش برای افزودن داده به جدول دیتابیس باعث نقض محدودیت‌های اطلاعاتی (اعتبارسنجی داده) می‌شود
+            # به عبارت دیگر، این خطاها به دلیل تلاش برای ایجاد رکوردهای تکراری یا نادرست در جداول رخ می‌دهند.
+
+            self.connection.close()
+            return "Username already exists. Please choose another one."
+
+class Logging_docter:
+    def __init__(self):
+        try:
+            self.connection = psycopg2.connect(
+                host="localhost",
+                port="5432",
+                database="hospital",
+                user="pouya",
+                password="1234",
+            )
+            self.cursor = self.connection.cursor()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        
+    def login(self, docter_id, medical_system_code):
+        self.cursor.execute(
+            "SELECT docter_id, medical_system_code FROM doctor where docter_id=%s ,(docter_id,medical_system_code)")
+        row = self.cursor.fetchone()
+
+        if row[0] == docter_id and row[1] == medical_system_code:
+            self.connection.close()
+            return "Login successful!"
+        else:
+            self.connection.close()
+            return "Login unsuccessful. Please check your username and password."
+
+    def register_doctor(self, docter_id, docter_name, age, working_day, salary):
+        try:
+            self.cursor.execute("INSERT INTO docter (docter_id, docter_name, docter_age, working_day, salary) VALUES (%s, %s,%s, %s,%s)",
+                                (docter_id, docter_name, age, working_day, salary))
+            self.connection.commit()
+            self.connection.close()
+            return "Registration successful!"
+        except psycopg2.IntegrityError:
+            self.connection.close()
+            return "Username already exists. Please choose an"
